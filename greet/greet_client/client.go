c713cd3278c6388055557da126c40e797ad50057
importantcoding <darrylrhardin@gmail.com>
2018-08-07 09:18:38 -0400

    adding server streaming

--- greet/greet_client/client.go
+++ greet/greet_client/client.go
@@ -20,7 +20,8 @@ func main() {
 
 	c := greetpb.NewGreetServiceClient(cc)
 	fmt.Printf("Created client: %f", c)
-	doUnary(c)
+	// doUnary(c)
+	doServerStreaming(c)
 }
 
 func doUnary(c greetpb.GreetServiceClient) {
@@ -36,4 +37,29 @@ func doUnary(c greetpb.GreetServiceClient) {
 		log.Fatalf("Error while calling Greet RPC: %v", err)
 	}
 	log.Printf("Response from Greet: %v", res.Result)
+}
+
+func doServerStreaming(c greetpb.GreetServiceClient) {
+	fmt.Println("Starting Streaming Server RPC...")
+	req := &greetpb.GreetManyTimesRequest{
+		Greeting: &greetpb.Greeting{
+			FirstName: "Darryl",
+			LastName:  "Hardin",
+		},
+	}
+	res, err := c.GreetManyTimes(context.Background(), req)
+	if err != nil {
+		log.Fatalf("Error while calling Greet RPC: %v", err)
+	}
+
+	for {
+		msg, err := res.Recv()
+		if msg == nil {
+			break
+		}
+		if err != nil {
+			log.Fatalf("Error with streaming response: %v", err)
+		}
+		log.Printf("Response from streaming server Greet: %v", msg)
+	}
 }
